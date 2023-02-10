015f6ed81af5b6dad9cd8e146192f537f6016ed5
Student User <student@workstation.lab.example.com>
2023-02-10 17:20:59 -0500

    fix parse error

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,7 +1,7 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 <<<<<<< HEAD
-print "Beautiful creature of this universe \n"
+print "Beautiful creature of this universe \n";
 =======
 print "Anu brin home the change ";
 >>>>>>> dec11738e7a8b1babad8fabdfeb28434d231c5d0
