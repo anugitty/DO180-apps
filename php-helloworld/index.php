6b597808f278e7de9fa015a80f8103b29b40703f
Student User <student@workstation.lab.example.com>
2023-02-10 17:07:50 -0500

    new cange with s2i-test

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,8 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+<<<<<<< HEAD
 print "Beautiful creature of this universe \n"
+=======
+print "Anu brin home the change ";
+>>>>>>> dec11738e7a8b1babad8fabdfeb28434d231c5d0
 ?>
